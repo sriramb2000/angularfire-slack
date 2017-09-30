518ef260609218ce9e5536a4f50d2631e4b762cc
dylansun18 <dylanswafflez@gmail.com>
2017-09-30 16:44:49 -0700

    indexhtml commentedjs

--- app/schools/schools.controller.js
+++ app/schools/schools.controller.js
@@ -15,5 +15,19 @@ angular.module('lemonApp')
         $state.go('schools');
       });
     };
-
+    // $scope.searchSchoolsn = function(){
+    //   var temp = $scope.dschools.length;
+		// 	var sumting = $scope.search;
+    //   for(var i = 0; i < temp; i++){
+		// 		var rip = $scope.dschools[i].substring(0, sumting.length);
+		// 		var areEqual = rip.toUpperCase() === sumting.toUpperCase();
+		// 		if(areEqual){
+		// 			document.getElementById(i + 1).style.display = "inline-block";
+    //
+		// 		}else {
+		// 			document.getElementById(i + 1).style.display = "none";
+    //
+		// 		}
+		// 	}
+		// }
   });
